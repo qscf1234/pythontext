4424899e0847d825af459c694f14c6347e53613e
qscf1234 <1059169458@qq.com>
2018-07-21 10:42:34 +0800

    Initial commit

---
+++ day06/set.py
@@ -0,0 +1,34 @@
+'''
+set:类似dict，是一组key的组合，不存储value
+
+本质：无序和无重复元素的集合
+'''
+# 创建
+# 创建set需要一个list或者tuple或者dict作为输入集合
+# 重复元素在set中会自动被过滤
+s1 = set([1, 2, 3, 4, 5, 4, 3])        # 列表
+print(s1)
+s2 = set((1, 2, 3, 4, 5, 4, 3))        # 元组
+print(s2)
+s3 = set({1: 'good', 2:'cool'})        # 字典
+print(s3)
+# 添加
+s4 = set([1, 2, 3, 4, 5])
+s4.add(6)
+s4.add(3) # 可以添加重复的但是不会用效果
+# s4.add([7, 8, 9]) # set的元素不能是列表，字典，因为他们可变
+s4.add((7, 8, 9))
+print(s4)
+
+# 插入整个list、tuple、字符串，打碎插入
+s5 = set([1, 2, 3, 4, 5])
+s5.update([6, 7, 8])
+s5.update('update')
+s5.update({11: 'good', 12: 'cool'})
+print(s5)
+
+# 删除
+s6 = set([1, 2, 3, 4, 5])
+s6.remove(3)
+print(s6)
+
